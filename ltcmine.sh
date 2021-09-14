65e1a3bbff35d467dd06c587ce97fb400d414f58
Mike3628 <89170401+Mike3628@users.noreply.github.com>
2021-09-14 08:32:59 +0800

    Create ltcmine.sh

---
+++ ltcmine.sh
@@ -0,0 +1,6 @@
+#ltccoin
+
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u LTC:MX4rv8Ez2xxrPiu7fx7kSXb5QSaT2n2Mbf -p x -k -a rx/0
